962c12698abc1a17de3674600a3bb6c30908c597
Mukta-Kakade <85670186+Mukta-Kakade@users.noreply.github.com>
2022-01-05 10:03:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name="Ranbir";
+var last_name="Kapoor";
+console.log(first_name);
+console.log(last_name);
+
+var a=10;
+var b=5;
+console.log(a);
+console.log(b);
+
+var c=25;
+var d=20;
+console.log(c);
+console.log(d);
